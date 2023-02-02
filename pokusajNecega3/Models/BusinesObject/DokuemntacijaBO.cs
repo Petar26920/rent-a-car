72c56b251077e846fbb08cfb5c866f04d557a454
Sladjan33020 <sladjan33020@its.edu.rs>
2023-02-02 16:52:01 +0100

    Dodao jos neke sitnice
    Popravio neke slovne greske i dodao lepsi ui

--- pokusajNecega3/Models/BusinesObject/DokuemntacijaBO.cs
+++ pokusajNecega3/Models/BusinesObject/DokuemntacijaBO.cs
@@ -9,11 +9,14 @@ namespace pokusajNecega3.Models.BusinesObject
     public class DokuemntacijaBO
     {
         [Required(ErrorMessage ="Morate da unesete ID")]
+        [RegularExpression("[0-9]+",ErrorMessage ="Mogu samo brojevi")]
         public int Idvozacke { get; set; }
 
         [Required(ErrorMessage ="Unesite ime")]
+        [RegularExpression("[a-zA-Z]+",ErrorMessage ="Samo mala i velika slova")]
         public string Ime { get; set; }
         [Required(ErrorMessage = "Unesite prezime")]
+        [RegularExpression("[a-zA-Z]+",ErrorMessage ="Samo mala i velika slova")]
         public string Prezime { get; set; }
 
         [Required(ErrorMessage = "Unesite datum rodjenja")]
